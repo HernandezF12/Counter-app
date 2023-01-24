3964a61957399657c2b9552fea3c19400b3f07ce
Felipe Laptop Hernandez <hernandezfjr15@gmail.com>
2023-01-24 15:12:56 -0600

    this is my first commit with most of the work completed already. Will only make small changes in the future.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Button from './button';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Button />
+  </React.StrictMode>
+);
